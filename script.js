c99baee7894e0e3f0b81aed385f7dd30664f07d0
ygordeoliveira <ygor90928@gmail.com>
2025-07-26 18:46:57 -0300

    JS file of project

---
+++ script.js
@@ -0,0 +1,34 @@
+var carta1 = {
+  nome: "Bulbasauro",
+  atributos: {
+    ataque: 7,
+    defesa: 8,
+    magia: 6
+  }
+};
+
+var carta2 = {
+  nome: "Darth Vader",
+  atributos: {
+    ataque: 9,
+    defesa: 8,
+    magia: 2
+  }
+};
+
+var carta3 = {
+  nome: "Shiryu de dragão",
+  atributos: {
+    ataque: 5,
+    defesa: 9,
+    magia: 10
+  }
+};
+
+var cartas = [carta1, carta2, carta3];
+var cartaMaquina;
+var cartaJogador;
+
+function sortearCarta() {
+  console.log(cartas[0]);
+}
